07d8ef0c58fd33e155df8d005fd94c6fbc0e07c1
Mochichi2003 <moyurusuto@icloud.com>
2020-10-29 15:32:39 +0900

    わーい

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import "react-native-gesture-handler";
 
-import React, { useCallback } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React, { Component } from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
-import { Button, ThemeProvider } from "react-native-elements";
+import { AppLoading } from "expo";
+
+import * as Font from "expo-font";
+
+import { Entypo, Fontisto, MaterialIcons } from "@expo/vector-icons";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text,
+} from "native-base";
 
 const Stack = createStackNavigator();
-import { StyleSheet, Text, View, Linking } from "react-native";
+import { StyleSheet, View } from "react-native";
 // import { AppLoading } from "expo";
 // import { useDarkMode, DarkModeProvider } from "react-native-dark-mode";
 
@@ -18,139 +35,53 @@ import "dayjs/locale/ja";
 dayjs.locale("ja");
 
 // type ClockTypes = { timerID: any };
-
-class Clock extends React.Component {
-  timerID: any;
-  nowtime: string & number;
-  constructor(props: any) {
-    super(props);
-    this.state = { nowtime: this.nowtimeis() };
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 10);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      nowtime: this.nowtimeis(),
-    });
-  }
-
-  nowtimeis() {
-    return dayjs().format("YYYY/MM/DD hh:mm:ss");
-  }
-
+export default class AnatomyExample extends Component {
   render() {
     return (
-      <View>
-        <ThemeProvider>
-          <Button
-            onPress={() => {
-              console.log("てすと" + dayjs().format("YYYY/MM/DD hh:mm:ss:SSS"));
-              alert("わあああああああ");
-            }}
-            title="こんにちは"
-            // color="#0092ca"
-            accessibilityLabel="Learn more about this purple button"
-            style={{
-              margin: 5,
-            }}
-          />
-        </ThemeProvider>
-
-        <Text style={styles.times}>{this.state.nowtime}</Text>
-      </View>
+      <Container>
+        <Header style={styles.header}>
+          <Left>
+            {/* <Button transparent> */}
+            {/* <Icon name="menu" /> */}
+            {/* </Button> */}
+          </Left>
+          <Body>
+            <Title>Home</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>{/* ここにメインコンテンツを書く */}</Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Fontisto name="home" size={24} color="black" />
+              <Text style={styles.futter_button_text}>Home</Text>
+            </Button>
+            <Button full>
+              <Entypo name="back-in-time" size={24} color="black" />
+              <Text style={styles.futter_button_text}>Times</Text>
+            </Button>
+            <Button full>
+              <MaterialIcons name="settings" size={24} color="black" />
+              <Text style={styles.futter_button_text}>Settings</Text>
+            </Button>
+            <Button full>
+              <Entypo name="calendar" size={24} color="black" />
+              <Text style={styles.futter_button_text}>Calendar</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
-
-const supportedURL = "https://google.com";
-
-const unsupportedURL = "slack://open?team=123456";
-
-const OpenURLButton = ({ url, children }) => {
-  const handlePress = useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
-      await Linking.openURL(url);
-    } else {
-      // Alert.alert(`Don't know how to open this URL: ${url}`);
-    }
-  }, [url]);
-
-  return <Button title={children} onPress={handlePress} />;
-};
-
-const OpenSettingsButton = ({ children }) => {
-  const handlePress = useCallback(async () => {
-    // Open the custom settings if the app has one
-    await Linking.openSettings();
-  }, []);
-
-  return <Button title={children} onPress={handlePress} />;
-};
-
-class MymainComponent extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar />
-        {/* <TextInput style={styles.textinputForm} defaultValue="Name me!" /> */}
-
-        {/* <Text>只今の時間</Text> */}
-        {/* <Text style={styles.times}>{dayjs().format("YYYY/MM/DD hh:mm:ss")}</Text> */}
-        {/* <Textinput /> */}
-        <Clock />
-        <OpenURLButton url="https://twitter.com/home">
-          url開くんだよ
-        </OpenURLButton>
-        <OpenSettingsButton>設定を開くんだよ</OpenSettingsButton>
-      </View>
-    );
-  }
-}
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <>
-      <Button
-        title="えへへへのへ"
-        onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-      />
-      <MymainComponent />
-    </>
-  );
-};
-const ProfileScreen = () => {
-  return <Text>This is Jane&apos;s profile</Text>;
-};
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
-
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#e4f1fe",
+  },
+  futter_button_text: {
+    fontSize: 14,
+  },
   button: {
     backgroundColor: "#f00",
   },
