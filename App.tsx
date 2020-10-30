e0917f0083d72923b1394909c331ec82ffc12433
Mochichi2003 <moyurusuto@icloud.com>
2020-10-31 00:35:23 +0900

    色々変えた

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { AppLoading } from "expo";
 
 import * as Font from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
 
 import { Entypo, Fontisto, MaterialIcons } from "@expo/vector-icons";
 import {
@@ -25,7 +26,10 @@ import {
 } from "native-base";
 
 const Stack = createStackNavigator();
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Switch, Share } from "react-native";
+
+import Debugsimport from "debug";
+const debug = Debugsimport("dev:buttons");
 // import { AppLoading } from "expo";
 // import { useDarkMode, DarkModeProvider } from "react-native-dark-mode";
 
@@ -34,7 +38,58 @@ import "dayjs/locale/ja";
 
 dayjs.locale("ja");
 
+const Fotters = () => {
+  return (
+    <Footer>
+      <FooterTab>
+        <Button
+          full
+          onPress={() => {
+            console.log("ボタンを押したよhome");
+            debug("ボタンを押したよhome");
+          }}
+        >
+          <Fontisto name="home" size={24} color="black" />
+          <Text style={styles.futter_button_text}>Home</Text>
+        </Button>
+        <Button
+          full
+          onPress={() => {
+            console.log("ボタンを押したよTimes");
+            debug("ボタンを押したよTimes");
+          }}
+        >
+          <Entypo name="back-in-time" size={24} color="black" />
+          <Text style={styles.futter_button_text}>Times</Text>
+        </Button>
+        <Button
+          full
+          onPress={() => {
+            console.log("ボタンを押したよSettings");
+            debug("ボタンを押したよSettings");
+          }}
+        >
+          <MaterialIcons name="settings" size={24} color="black" />
+          <Text style={styles.futter_button_text}>Settings</Text>
+        </Button>
+        <Button
+          full
+          onPress={() => {
+            console.log("ボタンを押したよCalendar");
+            debug("ボタンを押したよCalendar");
+          }}
+        >
+          <Entypo name="calendar" size={24} color="black" />
+          <Text style={styles.futter_button_text}>Calendar</Text>
+        </Button>
+      </FooterTab>
+    </Footer>
+  );
+};
+
 // type ClockTypes = { timerID: any };
+const isEnabled = true;
+
 export default class AnatomyExample extends Component {
   render() {
     return (
@@ -46,35 +101,37 @@ export default class AnatomyExample extends Component {
             {/* </Button> */}
           </Left>
           <Body>
-            <Title>Home</Title>
+            <Title>ホーム画面だよ</Title>
           </Body>
           <Right />
         </Header>
         <Content>{/* ここにメインコンテンツを書く */}</Content>
-        <Footer>
+        <Fotters />
+        {/* <Footer>
           <FooterTab>
-            <Button full>
+            <Button full onPress={() => {console.log("ボタンを押したよ")}}>
               <Fontisto name="home" size={24} color="black" />
               <Text style={styles.futter_button_text}>Home</Text>
             </Button>
-            <Button full>
+            <Button full onPress={() => {console.log("ボタンを押したよ")}}>
               <Entypo name="back-in-time" size={24} color="black" />
               <Text style={styles.futter_button_text}>Times</Text>
             </Button>
-            <Button full>
+            <Button full onPress={() => {console.log("ボタンを押したよ")}}>
               <MaterialIcons name="settings" size={24} color="black" />
               <Text style={styles.futter_button_text}>Settings</Text>
             </Button>
-            <Button full>
+            <Button full onPress={() => {console.log("ボタンを押したよ")}}>
               <Entypo name="calendar" size={24} color="black" />
               <Text style={styles.futter_button_text}>Calendar</Text>
             </Button>
           </FooterTab>
-        </Footer>
+        </Footer> */}
       </Container>
     );
   }
 }
+
 const styles = StyleSheet.create({
   header: {
     backgroundColor: "#e4f1fe",
